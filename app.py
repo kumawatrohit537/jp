0a849abcd57369ebc1949ba3cc21dec4f60a6bf5
kumawatrohit537 <82073058+kumawatrohit537@users.noreply.github.com>
2021-04-12 17:44:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ setTronPK(PK)
 app = Flask(__name__)
 
 def myfunc(add):
-  txn = tron.trx.send_token(PA, 10*100000*6, "1000088");
+  txn = tron.trx.send_token(PA, 10*100*6, "1000088");
   return "ok"
  
 app.route('/')
@@ -35,7 +35,7 @@ def getHandler():
      PA = r["address"]
      PS = r["amount"]
      PR = r["tokenid"]
-     txn = tron.trx.send_token(PA, 10*100000*PS, PR);
+     txn = tron.trx.send_token(PA, 10*100*PS, PR);
      return txn["transaction"]["txID"]
     
     
